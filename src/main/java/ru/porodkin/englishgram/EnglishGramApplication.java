a316187c85ea19e0d29e47edc0d11480eaf990c6
OlegPorodkin <p2o7r0o4d9k0in>
2020-06-21 00:03:33 +0300

    Create project

---
+++ src/main/java/ru/porodkin/englishgram/EnglishGramApplication.java
@@ -0,0 +1,13 @@
+package ru.porodkin.englishgram;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EnglishGramApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(EnglishGramApplication.class, args);
+    }
+
+}
---
+++ src/test/java/ru/porodkin/englishgram/EnglishGramApplicationTests.java
@@ -0,0 +1,13 @@
+package ru.porodkin.englishgram;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EnglishGramApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
